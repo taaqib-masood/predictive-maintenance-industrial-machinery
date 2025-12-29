d3567a67cb8fa3827f513a12c25185ffc454914b
taaqib-masood <taaqib.masood@icloud.com>
2025-12-30 00:36:53 +0400

    Update lstm_model.py

--- src/src/lstm_model.py
+++ src/src/lstm_model.py
@@ -1,1 +1,26 @@
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+
+class LSTMModel:
+    def __init__(self, input_shape):
+        self.model = Sequential([
+            LSTM(64, input_shape=input_shape, return_sequences=True),
+            Dropout(0.2),
+            LSTM(32),
+            Dropout(0.2),
+            Dense(1)
+        ])
+        self.model.compile(optimizer='adam', loss='mse')
+    
+    def train(self, X_train, y_train, epochs=50, batch_size=32, validation_data=None):
+        self.model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_data=validation_data)
+    
+    def predict(self, X):
+        return self.model.predict(X)
+    
+    def save_model(self, filepath):
+        self.model.save(filepath)
+    
+    def load_model(self, filepath):
+        self.model = load_model(filepath)
 
